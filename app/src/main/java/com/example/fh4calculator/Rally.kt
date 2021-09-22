6574daee0cce8de8b9ac834eabf32c6621c38bbb
YompoFR <ivanropero97@gmail.com>
2021-09-22 13:45:19 +0200

    App created

--- app/src/main/java/com/example/fh4calculator/Rally.kt
+++ app/src/main/java/com/example/fh4calculator/Rally.kt
@@ -4,8 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.btnCalculate
 import kotlinx.android.synthetic.main.activity_rally.*
 import java.lang.NumberFormatException
 
