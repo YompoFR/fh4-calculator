d1feceb5bca92200935be63082a5940720db46f1
IVAN <ivanropero97@gmail.com>
2022-06-13 16:51:42 +0200

    Reorder everything

---
+++ app/src/main/java/com/example/fh4calculator/Result.kt
@@ -0,0 +1,116 @@
+package com.example.fh4calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_result.*
+import kotlin.math.abs
+
+class Result : AppCompatActivity() {
+
+    // Max and min values
+    var springsMaxValue = 0.0
+    var springsMinValue = 0.0
+
+    var barsMaxValue = 65.0
+    var barsMinValue = 1.0
+
+    var dampingMaxValue = 0.0
+    var dampingMinValue = 0.0
+
+    // Final values
+    var frontSpringsValue = 0.0
+    var rearSpringsValue = 0.0
+
+    var frontBarsValue = 0.0
+    var rearBarsValue = 0.0
+
+    var frontDampingValue = 0.0
+    var rearDampingValue = 0.0
+
+    var frontWeight = 0.0
+    var rearWeight = 0.0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+
+        // Checking info and assigning values
+        checkIntentInfo()
+
+        // Calculating values..
+        calculateValues()
+
+        // Filling fields
+        fillFields()
+
+    }
+
+    private fun checkIntentInfo() {
+        if (intent.extras != null) {
+            //selectedButton = intent.getIntExtra("button", 0)
+            springsMaxValue = intent.getDoubleExtra("springsMax", 0.0)
+            springsMinValue = intent.getDoubleExtra("springsMin", 0.0)
+            dampingMaxValue = intent.getDoubleExtra("dampingMax", 20.0)
+            dampingMinValue = intent.getDoubleExtra("dampingMin", 3.0)
+            frontWeight = intent.getDoubleExtra("frontWeight", 0.0)
+        }
+    }
+
+    private fun calculateValues() {
+        calculateRearWeight()
+        calculateFrontWeight()
+        calculateBars()
+        calculateSprings()
+        calculateDamping()
+    }
+
+    private fun calculateRearWeight() {
+        rearWeight = (frontWeight - 100)
+        rearWeight = abs(rearWeight) / 100
+    }
+
+    private fun calculateFrontWeight() {
+        frontWeight /= 100
+    }
+
+    private fun calculateBars() {
+        frontBarsValue = formula(barsMaxValue, barsMinValue, frontWeight)
+        rearBarsValue = formula(barsMaxValue, barsMinValue, rearWeight)
+    }
+
+    private fun calculateSprings() {
+        frontSpringsValue = formula(springsMaxValue, springsMinValue, frontWeight)
+        rearSpringsValue = formula(springsMaxValue, springsMinValue, rearWeight)
+    }
+
+    private fun calculateDamping() {
+        frontDampingValue = formula(dampingMaxValue, dampingMinValue, frontWeight)
+        rearDampingValue = formula(dampingMaxValue, dampingMinValue, rearWeight)
+    }
+
+    private fun formula(maxValue: Double, minValue: Double, weight: Double): Double {
+        return (maxValue - minValue) * weight + minValue
+    }
+
+    private fun fillFields() {
+        fillSprings()
+        fillBars()
+        fillDamping()
+    }
+
+    private fun fillSprings() {
+        tvFrontSpringsValue.text = frontSpringsValue.toString()
+        tvRearSpringsValue.text = rearSpringsValue.toString()
+    }
+
+    private fun fillBars() {
+        tvFrontBarsValue.text = frontBarsValue.toString()
+        tvRearBarsValue.text = rearBarsValue.toString()
+    }
+
+    private fun fillDamping() {
+        tvFrontDampingValue.text =  String.format("%.2f", frontDampingValue)
+        tvRearDampingValue.text =  String.format("%.2f", rearDampingValue)
+    }
+}
